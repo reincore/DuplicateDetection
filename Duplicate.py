8f882b58285a56849cb617702dc6863dc4b00fa2
Deniz Sağlam <reincore@users.noreply.github.com>
2016-03-30 03:14:34 +0200

    Added files via upload
    Includes the new function to load and get info out of the JSON files as well as the fully functional first one. 
    The second new function has to be fully completed, i.e. it should keep track of the keys and scales of every duplicate of a given song.

--- Duplicate.py
+++ Duplicate.py
@@ -4,16 +4,23 @@ import os, sys
 inputDir='ab-duplicates100-2016-03-02/'
 fileCount = 0
 fname_MBID = ""
-fname_List = [] # List of fnames of songs and duplicates
+fname_List = [] # List of file names
 path_Array = [] # List of full paths of the duplicates of a given song
 
-#fDict = json.load(open(str('ab-duplicates1000-2016-03-02/00/00c47ea6-3a10-4a32-b1f1-990ac756c6a0-0.json')))
 #dataDetails={}
 #dataDetails[cname][sname] ={'number':'0','feature':fDict}
 #[u'lowlevel', u'tonal', u'rhythm', u'metadata']
 # fDict['lowlevel'].keys()
 # fDict['tonal']['key_key']
 # fDict['tonal']['key_scale']
+
+def json_Read(path_Array):
+	song_Key = "" 
+	for n in path_Array:
+		fDict = json.load(open((str(n))))
+		song_Key = str(fDict['tonal']['key_key']) + str(fDict['tonal']['key_scale'])
+		#print song_Key
+	return
 
 def file_trav(inputDir):
 	#print inputDir
@@ -34,20 +41,24 @@ def file_trav(inputDir):
 				root_Name = root_Name[:-5]
 				#print(root_Name) + " ---> Root Name" #The root part of the file name without duplicate number and .json part.
 
-				if (len(fname_List) >> 2) and (fname_List [-1] == fname_List [-2]):
+				print (len(path_Array))
+				if (len(path_Array) >= 3 and (fname_List [-1] == fname_List[-2])):
 					#call new function to deal with JSON files
-					print "Same file as previous"	
+					json_Read(path_Array)
+					#print "Same file as previous"	
 					path_Array.append(str(path) + "/" + str(fname))
 
-				elif (len(fname_List) >> 2 and fname_List [-1] != fname_List [-2]):
+				elif (len(path_Array) >= 3 and (fname_List [-1] != fname_List [-2])):
 					print "New file"
 
-				#call function
-				#clear array
-				#append current path
-				elif (len(fname_List) << 3):
+				#--- call new function for JSON file opening here ---
+
+					path_Array = []
 					path_Array.append(str(path) + "/" + str(fname))
-					print path_Array
+					#print path_Array # one element
+				elif (len(path_Array) <= 2):
+					path_Array.append(str(path) + "/" + str(fname))
+					#print path_Array #two and three elements
 
 file_trav(inputDir)
 print str(fileCount) + " -->  File Count"
